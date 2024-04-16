99a53fc1053b16fdc2b31a30b506ee0d27f44aba
Ali_Ismail <ali.esmail.ghareeb@gmail.com>
2024-04-16 23:48:11 +0200

    [66] [Time Beats: 100.00%] [Memory Beats: 27.14%] - LeetPush

---
+++ 66-Plus-One.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int[] plusOne(int[] digits) {
+        int n=digits.length;
+        for(int i=n-1;i>=0;i--){
+            if(digits[i] < 9){
+                digits[i]++;
+                return digits;
+            }else if(digits[i] == 9){
+                digits[i]=0;
+            }
+        }
+        int[] newNumbers = new int[n+1];
+        newNumbers[0] = 1;
+        return newNumbers;
+
+    }
+}
