b22020052140d3da89babd2f4484805402cea92c
Ali_Ismail <ali.esmail.ghareeb@gmail.com>
2024-04-20 08:52:01 +0200

    [383] [Time Beats: 31.87%] [Memory Beats: 44.72%] - LeetPush

---
+++ 383-Ransom-Note.java
@@ -0,0 +1,25 @@
+class Solution {
+    public boolean canConstruct(String ransomNote, String magazine) {
+        
+        HashMap<Character, Integer> map = new HashMap<>();
+        for (char m : magazine.toCharArray()) {
+            if (map.containsKey(m) == false) {
+                map.put(m, 1);
+            } else {
+                map.put(m, 1 + map.get(m));
+            }
+        }
+        for (char r : ransomNote.toCharArray()) {
+            if (map.containsKey(r)) {
+                if (map.get(r) > 0) {
+                    map.put(r, map.get(r) - 1);
+                } else {
+                    return false;
+                }
+            } else {
+                return false;
+            }
+        }
+        return true;
+    }
+}
