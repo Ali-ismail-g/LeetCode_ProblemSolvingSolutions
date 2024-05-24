fbdc215f8999c6c977dae164355e0a0de5b222c9
Ali_Ismail <ali.esmail.ghareeb@gmail.com>
2024-05-24 15:09:59 +0300

    [125] [Time Beats: 40.91%] [Memory Beats: 36.86%] - LeetPush

--- 125-Valid-Palindrome.java
+++ 125-Valid-Palindrome.java
@@ -2,11 +2,19 @@ class Solution {
     public boolean isPalindrome(String s) {
         s=s.toLowerCase();
         s=s.replaceAll("[^a-z0-9]+","");
-        StringBuilder pal = new StringBuilder();
-        for(int i=s.length()-1;i>=0;i--){
-            pal.append(s.charAt(i));
+        if(s.length()==0){
+            return true;
         }
-        return pal.toString().equals(s);
+        char a_pointer=s.charAt(0);
+        char b_pointer=s.charAt(s.length()-1);
+        for(int i=0;i<s.length();i++){
+            if(a_pointer!=b_pointer){
+                return false;
+            }
+            b_pointer=s.charAt(s.length()-1-i);
+                a_pointer=s.charAt(0+i);
+        }
+        return true;
         
     }
 }
