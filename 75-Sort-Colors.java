8333acc32b412cd1da8a35a2f6a41cfb2818d801
Ali_Ismail <ali.esmail.ghareeb@gmail.com>
2024-07-09 14:48:48 +0300

    [75] [Time Beats: 11.33%] [Memory Beats: 7.53%] - LeetPush

--- 75-Sort-Colors.java
+++ 75-Sort-Colors.java
@@ -1,13 +1,18 @@
 class Solution {
     public void sortColors(int[] nums) {
-        for(int i=0;i<nums.length;i++){
-            for(int j=i+1;j<nums.length;j++){
-                if(nums[j]<nums[i]){
-                    int temp = nums[i];
-                    nums[i] = nums[j];
-                    nums[j] = temp;
-                }
-            }
+        int zeroes=0;int ones=0;int n=nums.length;
+        for(int num:nums){
+            if(num==0) zeroes++;
+            else if(num==1) ones++;
+        }
+        for(int i=0;i<zeroes;i++){
+            nums[i] = 0;
+        }
+        for(int i=zeroes;i<zeroes+ones;i++){
+            nums[i] = 1;
+        }
+        for(int i=zeroes+ones;i<n;i++){
+            nums[i] = 2;
         }
         System.out.println(Arrays.toString(nums));
     }
