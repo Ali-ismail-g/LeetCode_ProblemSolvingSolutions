d70f875afd5ab462b0408f12ddebb8841afdeb45
Ali_Ismail <ali.esmail.ghareeb@gmail.com>
2024-12-07 19:55:30 +0200

    [1] [Time Beats: 60.57%] [Memory Beats: 7.32%] - LeetPush

--- 1-Two-Sum.java
+++ 1-Two-Sum.java
@@ -1,16 +1,21 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        int num1=0,num2=0;
-        for(int i=0;i<nums.length;i++){
-            for(int j=i+1;j<nums.length;j++){
-                if(nums[i]+nums[j] == target){
-                num1+=i;
-                num2+=j;
-                break;
-            }
+         Map<Integer, Integer> numMap = new HashMap<>();
+        int n = nums.length;
+
+        // Build the hash table
+        for (int i = 0; i < n; i++) {
+            numMap.put(nums[i], i);
+        }
+
+        // Find the complement
+        for (int i = 0; i < n; i++) {
+            int complement = target - nums[i];
+            if (numMap.containsKey(complement) && numMap.get(complement) != i) {
+                return new int[]{i, numMap.get(complement)};
             }
         }
-        int[] intArray = {num1,num2};
-        return intArray; 
+
+        return new int[]{}; // No solution found
     }
 }
